379992c6aa3b2d07fc8c67a78b9abac7c04e7bb7
Blake Wright <blake@ip-172-26-11-113.us-east-2.compute.internal>
2023-12-08 08:49:06 +0000

    songs

--- public/songs.js
+++ public/songs.js
@@ -1,7 +1,238 @@
 let songs = [
-    {
-      url: "https://soundcloud.com/hans-zimmer-official/shes-not-real",
-      answer: "Hans Zimmer - She's Not Real (Interstellar)",
-    },
-  ];
-  
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/dreaming-of-the-crash?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer:
+      "Interstellar () [Expanded Edition] - Dreaming of the Crash - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/cornfield-chase?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer:
+      "Interstellar () [Expanded Edition] - Cornfield Chase - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/dust?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - Dust - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/day-one?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer:
+      "Interstellar () [Expanded Edition] - Day One (Interstellar Theme) - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/stay?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - Stay - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/message-from-home?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer:
+      "Interstellar () [Expanded Edition] - Message from Home - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/the-wormhole?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - The Wormhole - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/mountains?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - Mountains - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/afraid-of-time?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - Afraid of Time - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/a-place-among-the-stars?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer:
+      "Interstellar () [Expanded Edition] - A Place Among the Stars - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/running-out?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - Running Out - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/im-going-home?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - I'm Going Home - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/coward?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - Coward - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/detach?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - Detach - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/s-t-a-y?in=hans-zimmer-official/sets/interstellar-original-motion-3",
+    answer: "Interstellar () [Expanded Edition] - S.T.A.Y. - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/national-treasure-suite?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - National Treasure Suite - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/ben?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Ben - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/finding-charlotte?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Finding Charlotte - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/library-of-congress?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Library of Congress - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/preparation-montage?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Preparation Montage - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/arrival-at-national-archives?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Arrival at National Archives - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/the-chase?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - The Chase - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/declaration-of-independence?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Declaration of Independence - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/foot-chase?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Foot Chase - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/spectacle-discovery?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Spectacle Discovery - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/interrogation?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Interrogation - Trevor Rabin",
+  },
+  {
+    url: "https://soundcloud.com/trevor-rabin/treasure?in=trevor-rabin/sets/national-treasure-1",
+    answer: "National Treasure - Treasure - Trevor Rabin",
+  },
+
+  {
+    url: "https://soundcloud.com/thebellestars/iko-iko?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - Iko Iko - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/johnny-clegg-savuka/scatterlings-of-africa?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - Scatterlings of Africa - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/the-delta-rhythm-boys/dry-bones?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - Dry Bones - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/ettajamesofficial/at-last-2?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - At Last - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/ian-gillan-official/lonely-avenue?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - Lonely Avenue - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/bananarama/nathan-jones?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - Nathan Jones - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/leaving-wallbrook-on-the-road?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - Leaving Wallbrook/On The Road - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/hans-zimmer-official/las-vegas-end-credits?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - Las Vegas/End Credits - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/robwasserman/stardust?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - Stardust - Hans Zimmer",
+  },
+  {
+    url: "https://soundcloud.com/lou-christie-official/beyond-the-blue-horizon?in=hans-zimmer-official/sets/rain-man-original-motion",
+    answer:
+      "Rain Man: Original Motion Picture Soundtrack - Beyond The Blue Horizon - Hans Zimmer",
+  },
+
+  {
+    url: "https://soundcloud.com/cast-frozen/frozen-heart?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - Frozen Heart - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/kristen-bell-official/do-you-want-to-build-a-snowman?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - Do You Want to Build a Snowman? - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/kristen-bell-official/for-the-first-time-in?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - For the First Time in Forever - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/kristen-bell-official/love-is-an-open-door?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - Love Is an Open Door - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/idinamenzel/let-it-go-from-frozen?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - Let It Go - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/jonathan-groff-official/reindeer-s-are-better-than?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - Reindeer(s) Are Better Than People - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/joshgad/in-summer?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - In Summer - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/kristen-bell-official/for-the-first-time-in-forever?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - For the First Time in Forever (Reprise) - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/maiawilsonofficial/fixer-upper?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - Fixer Upper - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/ddlovato/let-it-go-single-version?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen - Demi Lovato - Let It Go - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/martina-stoessel-official/libre-soy-single-version?in=christophe-beck-music/sets/frozen-514",
+    answer: "Frozen -Libre Soy (Single Version) - Christophe Beck",
+  },
+  {
+    url: "https://soundcloud.com/christophe-beck-music/vuelie?in=christophe-beck-music/sets/frozen-514",
+    answer:
+      'Frozen -Vuelie [feat. Cantus] - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/christophe-beck-music/elsa-and-anna?in=christophe-beck-music/sets/frozen-514",
+    answer: 'Frozen -Elsa and Anna - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/christophe-beck-music/the-trolls?in=christophe-beck-music/sets/frozen-514",
+    answer: 'Frozen -The Trolls - Christophe Beck',
+  },
+  {
+    url: "https://soundcloud.com/christophe-beck-music/coronation-day?in=christophe-beck-music/sets/frozen-514",
+    answer: 'Frozen -Coronation Day - Christophe Beck',
+  },
+];
